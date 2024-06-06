1d0cdbfc7536441b11e40fbf5d2b2db0ced54638
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2024-06-05 23:51:34 -0400

    Update CryptoQuestTheShardsOfGenesisCollectionNFTs.sol

--- src/smartcontracts/CryptoQuestTheShardsOfGenesisCollectionNFTs.sol
+++ src/smartcontracts/CryptoQuestTheShardsOfGenesisCollectionNFTs.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 // Compatible with OpenZeppelin Contracts ^5.0.0
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.25;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC1155/ERC1155Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
