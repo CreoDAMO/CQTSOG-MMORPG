61f62d0e8a5478f7aecb7b1827109d33c73728e4
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2024-06-21 00:00:03 +0000

    git update all

---
+++ cryptoquest-dapp/src/utils/web3Modal.js
@@ -0,0 +1,19 @@
+import Web3Modal from "web3modal";
+import { ethers } from "ethers";
+import { InjectedConnector } from "@web3-react/injected-connector";
+import { Web3Provider } from "@ethersproject/providers";
+
+export const injected = new InjectedConnector({
+    supportedChainIds: [1, 3, 4, 5, 42]
+});
+
+export const web3Modal = new Web3Modal({
+    cacheProvider: true,
+    providerOptions: {}
+});
+
+export const getLibrary = (provider) => {
+    const library = new Web3Provider(provider);
+    library.pollingInterval = 12000;
+    return library;
+};
