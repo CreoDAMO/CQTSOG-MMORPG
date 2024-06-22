2b75ac71b528c3e6f3d25b101b2cb199ac840678
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2024-06-22 11:50:14 -0400

    Update vite.config.js

--- cryptoquest-dapp/vite.config.js
+++ cryptoquest-dapp/vite.config.js
@@ -1,13 +1,6 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
-  server: {
-    open: true,
-  },
 });
