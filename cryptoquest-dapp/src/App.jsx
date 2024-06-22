197f156074153eb118266faed415834ffe454087
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2024-06-22 11:49:27 -0400

    Update App.jsx

--- cryptoquest-dapp/src/App.jsx
+++ cryptoquest-dapp/src/App.jsx
@@ -1,18 +1,21 @@
-// src/App.jsx
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import './App.css';
 import Header from './components/Header';
 import Features from './components/Features';
+import Home from './components/Home';
+import Game from './components/Game';
 
 function App() {
   const { t } = useTranslation();
   return (
     <div>
       <Header />
+      <Home />
       <main>
         <h1>{t('Welcome')}</h1>
         <Features />
+        <Game />
         {/* Add other components as needed */}
       </main>
     </div>
