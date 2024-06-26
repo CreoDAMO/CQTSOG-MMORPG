c1247847dbaaffa040d63a2def49568f376ad700
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2024-06-26 19:16:15 -0400

    Update SmartContractManager.h

--- cryptoquest/cryptoquestmmorpg-dapp/src/ccp/SmartContractManager.h
+++ cryptoquest/cryptoquestmmorpg-dapp/src/ccp/SmartContractManager.h
@@ -1,7 +1,35 @@
-#include <map>
-#include <web3cpp.h>
+// SmartContractManager.h
+
+#ifndef SMARTCONTRACTMANAGER_H
+#define SMARTCONTRACTMANAGER_H
+
+#include <string>
+#include <vector>
+#include <json/json.h> // Make sure to include the JSON library
 
 class SmartContractManager {
 public:
-    SmartContractManager(const std::string& provider);
-    void addContract(const std::string& name, const std::string& address, const std::string& abi
+    // Constructor
+    SmartContractManager(const std::string& contractAddress, const std::string& abi);
+
+    // Destructor
+    ~SmartContractManager();
+
+    // Function to deploy a new contract
+    std::string deployContract(const std::string& bytecode, const std::string& senderPrivateKey);
+
+    // Function to call a smart contract function
+    Json::Value callFunction(const std::string& functionName, const std::vector<std::string>& params);
+
+    // Function to send a transaction to the contract
+    std::string sendTransaction(const std::string& functionName, const std::vector<std::string>& params, const std::string& senderPrivateKey);
+
+    // Function to listen for events
+    void listenForEvents(const std::string& eventName);
+
+private:
+    std::string contractAddress;
+    std::string abi;
+};
+
+#endif // SMARTCONTRACTMANAGER_H
