ea433d0065e4555a3671ffcaa5822542700b6d08
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2024-07-20 11:28:14 -0400

    Update App.test.js

--- cryptoquestmmorpg-dapp/src/App.test.js
+++ cryptoquestmmorpg-dapp/src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders CryptoQuest DApp header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText(/CryptoQuest DApp/i);
+  expect(headerElement).toBeInTheDocument();
 });
+
