c5080ae3d5864b007f259052294bba2afaf9cfd7
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2024-07-21 19:12:44 -0400

    Delete cryptoquestmmorpg-dapp/src/App.test.js

--- cryptoquestmmorpg-dapp/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders CryptoQuest DApp header', () => {
-  render(<App />);
-  const headerElement = screen.getByText(/CryptoQuest DApp/i);
-  expect(headerElement).toBeInTheDocument();
-});
-
