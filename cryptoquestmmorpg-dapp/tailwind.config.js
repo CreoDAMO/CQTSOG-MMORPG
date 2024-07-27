459677af344293a478ed7509a876ae3871f5d5fb
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2024-07-27 17:13:53 +0000

    git update

--- cryptoquestmmorpg-dapp/tailwind.config.js
+++ cryptoquestmmorpg-dapp/tailwind.config.js
@@ -1,12 +1,10 @@
 // tailwind.config.js
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Ensure all your source files are included here
+  ],
   theme: {
     extend: {},
   },
-  variants: {
-    extend: {},
-  },
   plugins: [],
-};
+}
